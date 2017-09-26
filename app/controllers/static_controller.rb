619cda8570536bb14deb89fc3d3c7b10461d9827
sonal <sonal@buzz4health.com>
2017-09-26 22:47:03 +0530

    inclusion of pages

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,3 +1,90 @@
 class StaticController < ApplicationController
+
+
+
+	def index 
+
+
+	end 
+
+	def show 
+
+
+	end 
+
+
+    def animal
+
+    end 
+
+
+    def bird 
+
+    end 
+
+
+    def reptile
+
+    end 
+
+
+    def nocturnalhouse
+
+    end  
+
+    def serpentarium
+
+    end 
+
+
+    def aquarium
+
+    end 
 	
+
+	def interpretation_center
+
+	end 
+
+	def start_art_museum 
+
+	end 
+
+	def floraldiversity
+
+	end 
+
+	def tender
+
+	end 
+
+    def tickets_rates_timming
+    	
+    end
+
+    def main_animal_at_zoo
+
+    end 
+
+    def zoo_map
+
+    end 
+    
+    def does_and_dont
+    end 
+
+    def certificate
+    end 
+
+    def terms_condition
+    end 
+
+    def veretinary_care
+    end 
+
+    def animal_detail
+
+    end 
+
+
 end
